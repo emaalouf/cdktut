4554a2865f7a06b6d0bad7639f14f0d2ff7ae6d2
Elias Maalouf <egmaalouf@gmail.com>
2023-02-15 14:16:23 +0200

    First

---
+++ test/cdktut.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Cdktut from '../lib/cdktut-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdktut-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Cdktut.CdktutStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
