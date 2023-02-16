f29926a0dfb26996b871c1b21c38024869cfbfb7
Elias Maalouf <egmaalouf@gmail.com>
2023-02-16 11:18:21 +0200

    Fix

--- lib/controllers/taskController.ts
+++ lib/controllers/taskController.ts
@@ -1,3 +1,13 @@
 exports.getTasks = async function (event: any) {
-  console.log("request:", event);
+  var object = {
+    name: 'Mohamad',
+    age: 20,
+    email: 'efpyi@example.com',
+    phone: '08000000000',
+    address: 'Mohamad, Karachi',
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(object),
+  };
 };
--- lib/stacks/task-stack.ts
+++ lib/stacks/task-stack.ts
@@ -1,22 +1,46 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambda_node from "aws-cdk-lib/aws-lambda-nodejs";
+import * as iam from "aws-cdk-lib/aws-iam";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as gateway from "aws-cdk-lib/aws-apigateway";
 import * as path from "path";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class TaskStack extends cdk.Stack {
+  api: gateway.RestApi;
+  apiCustomAuthorizer: gateway.CfnAuthorizer
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
+    this.createApi();
     this.createTaskLambdafunction();
   }
-  createTaskLambdafunction() {
-    const helloworldfn = new lambda_node.NodejsFunction(this, "helloworldfn", {
-      functionName :       "helloworldfn",  
-      runtime: lambda.Runtime.NODEJS_16_X,
-      handler: "getTasks",
-      entry: path.join(__dirname, "../controllers/taskController.ts"),
-    });
+  createApi() {
+    this.api = new gateway.RestApi(this, "taskApi", {});
+    
   }
-}
+  createTaskLambdafunction() {
+    //1- create resource
+    const taskResource = this.api.root.addResource("get-task");
+
+    //2- create lambda function
+
+    const getTasksLambdaFn = new lambda_node.NodejsFunction(
+      this,
+      "helloworldfn",
+      {
+        functionName: "helloworldfn",
+        runtime: lambda.Runtime.NODEJS_16_X,
+        handler: "getTasks",
+        entry: path.join(__dirname, "../controllers/taskController.ts"),
+      }
+    );
+
+    //3- create method and integration
+    taskResource.addMethod(
+      "GET",
+      new gateway.LambdaIntegration(getTasksLambdaFn)
+    );
+  }
+  }
+
